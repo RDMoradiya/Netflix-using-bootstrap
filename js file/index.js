ee1bd64ef91b66f9377a09774573c0a0ad15d4c6
RAJ D. MORADIYA <110352291+RDMoradiya@users.noreply.github.com>
2022-12-17 00:56:17 +0530

    Add files via upload

--- js file/index.js
+++ js file/index.js
@@ -1,4 +1,4 @@
-function emailvalidation() {
+function emailvalidation1() {
   let emailid = document.getElementById("email");
   if (emailid.value == "" || emailid.value == null) {
     document.getElementById("eml").innerHTML = "Email is required.";
@@ -9,6 +9,21 @@ function emailvalidation() {
     return true;
   } else {
     document.getElementById("eml").innerHTML = "Invalid Email";
+    return false;
+  }
+}
+
+function emailvalidation2() {
+  let emailid = document.getElementById("email2");
+  if (emailid.value == "" || emailid.value == null) {
+    document.getElementById("eml2").innerHTML = "Email is required.";
+    return false;
+  } else if (
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailid.value)
+  ) {
+    return true;
+  } else {
+    document.getElementById("eml2").innerHTML = "Invalid Email";
     return false;
   }
 }
